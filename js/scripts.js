d58631d8b4e6cebcf5d55aea605cb19dc40d7f91
Eric Nambo <ericnambo@gmail.com>
2017-06-12 20:32:43 -0700

    Add bmi calc to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 var add = function(number1, number2) {
-	return number1+number2;
+	return number1 + number2;
 };
 
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
 
-alert(add(number1, number2));
+alert(result);
+
+//bmi calc
+
+/**
+
+var bmi = function(height, weight) {
+	return 703*(weight / (Math.pow(height, 2)));
+};
+
+var height = parseInt(prompt("Enter your height in inches:"));
+var weight = parseInt(prompt("Enter your weight in lbs:"));
+var result = bmi(height, weight);
+
+alert("Your BMI is " + result);
+
+**/
